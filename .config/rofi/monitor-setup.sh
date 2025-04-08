6d72b0b5a7f95541a4faaf6172c8174433d86101
K1EW <weerawatwmt@gmail.com>
2025-04-08 22:12:44 +0700

    add rofi monitor setup utility

---
+++ .config/rofi/monitor-setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+available_presets=("💻Single" "🏠Home")
+
+if [ "$ROFI_RETV" == "0" ]; then
+  # List available presets
+  printf "%s\n" "${available_presets[@]}"
+elif [ "$ROFI_RETV" == "1" ]; then
+  # Run the selected preset
+  case "$1" in
+    single)
+      xrandr --output eDP-1 --mode 1920x1080 --rate 144 --primary
+      xrandr --output HDMI-1 --off
+	  sleep 1
+      i3-msg restart > /dev/null
+      ;;
+    home)
+      xrandr --output HDMI-1 --mode 1920x1080 --rate 75 --primary
+      xrandr --output eDP-1 --mode 1920x1080 --rate 144 --left-of HDMI-1
+	  sleep 1
+      i3-msg restart > /dev/null
+      ;;
+  esac
+fi
