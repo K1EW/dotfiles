d08835f09d5ddf39508bd6c864f849ac1a6bbb01
K1EW <weerawatwmt@gmail.com>
2025-04-10 00:13:34 +0700

    edit rofi monitor presets selector

--- .config/rofi/monitor-setup.sh
+++ .config/rofi/monitor-setup.sh
@@ -8,17 +8,20 @@ if [ "$ROFI_RETV" == "0" ]; then
 elif [ "$ROFI_RETV" == "1" ]; then
   # Run the selected preset
   case "$1" in
-    single)
+    "💻Single")
       xrandr --output eDP-1 --mode 1920x1080 --rate 144 --primary
       xrandr --output HDMI-1 --off
-	  sleep 1
+      sleep 1
       i3-msg restart > /dev/null
       ;;
-    home)
+    "🏠Home")
       xrandr --output HDMI-1 --mode 1920x1080 --rate 75 --primary
       xrandr --output eDP-1 --mode 1920x1080 --rate 144 --left-of HDMI-1
-	  sleep 1
+      sleep 1
       i3-msg restart > /dev/null
+      ;;
+    *)
+      notify-send "Unknown preset: $1"
       ;;
   esac
 fi
