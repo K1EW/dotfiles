6fc8d5ef978cc45c2f90dec091ef841353c64a6c
K1EW <weerawatwmt@gmail.com>
2026-06-17 16:40:26 +0900

    neovim: enter floating window automatically when triggered

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -43,16 +43,20 @@ return {
             callback = function(event)
                 vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = event.buf, silent = true, desc = "Go to definition" })
                 vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = event.buf, silent = true, desc = "Go to declaration" })
-                vim.keymap.set("n", "K",  vim.lsp.buf.hover, { buffer = event.buf, silent = true, desc = "Hover documentation" })
+                vim.keymap.set("n", "K", function()
+                    for _ = 0, 1 do vim.lsp.buf.hover({ border = "rounded" }) end
+                end, { buffer = event.buf, silent = true, desc = "Hover documentation" })
                 vim.keymap.set("n", "gr", vim.lsp.buf.references, { buffer = event.buf, silent = true, desc = "Find references" })
                 vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { buffer = event.buf, silent = true, desc = "Rename symbol" })
                 vim.keymap.set("n", "<leader>sdf", function()
-                vim.diagnostic.open_float({
-                        border = "rounded",
-                        source = "if_many",
-                        header = "Line Diagnostics",
-                        prefix = "• ",
-                    })
+                    for _ = 0, 1 do
+                        vim.diagnostic.open_float({
+                            border = "rounded",
+                            source = "if_many",
+                            header = "Line Diagnostics",
+                            prefix = "• ",
+                        })
+                    end
                 end, { buffer = event.buf, silent = true, desc = "Show diagnostics window" })
                 vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { buffer = event.buf, silent = true, desc = "Code actions" })
             end,
